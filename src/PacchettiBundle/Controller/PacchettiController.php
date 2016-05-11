40d7ed1fcd3ceff59578ce1c76c496e8d2db33a2
antoniogaldo <antoniohendrix@hotmail.it>
2016-05-11 10:41:31 +0200

    form pacchetto

--- src/PacchettiBundle/Controller/PacchettiController.php
+++ src/PacchettiBundle/Controller/PacchettiController.php
@@ -4,13 +4,38 @@ namespace PacchettiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use PacchettiBundle\Entity\nuovo_pacchetto;
+use PacchettiBundle\Form\nuovo_pacchettoFormType;
 
 
 class PacchettiController extends Controller
 {
     
-    public function indexAction()
-    {
-        return $this->render('PacchettiBundle:Pacchetti:index.html.twig');
+    public function indexAction(Request $request)
+    
+    	{
+        $pacchetto = new nuovo_pacchetto();
+
+        
+        $form1 = $this->createForm(nuovo_pacchettoFormType::class, $pacchetto);
+
+        $form1->handleRequest($request);
+
+        if ($form1->isSubmitted() && $form1->isValid()) {
+
+            // Salvo cose.
+            $pacchetto = $form1->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($pacchetto);
+            $em->flush(); }
+
+
+        return $this->render('PacchettiBundle:Pacchetti:index.html.twig', array(
+        		'form1' => $form1->createView(),
+        	));
     }
+       
 }
---
+++ src/PacchettiBundle/Entity/nuovo_pacchetto.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace PacchettiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * nuovo_pacchetto
+ *
+ * @ORM\Table(name="nuovo_pacchetto")
+ * @ORM\Entity(repositoryClass="PacchettiBundle\Repository\nuovo_pacchettoRepository")
+ */
+class nuovo_pacchetto
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome_pacchetto", type="string", length=255)
+     */
+    private $nomePacchetto;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="data_inizio", type="datetime")
+     */
+    private $dataInizio;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="data_fine", type="datetime")
+     */
+    private $dataFine;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="servizi", type="string", length=255)
+     */
+    private $servizi;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nomePacchetto
+     *
+     * @param string $nomePacchetto
+     *
+     * @return nuovo_pacchetto
+     */
+    public function setNomePacchetto($nomePacchetto)
+    {
+        $this->nomePacchetto = $nomePacchetto;
+
+        return $this;
+    }
+
+    /**
+     * Get nomePacchetto
+     *
+     * @return string
+     */
+    public function getNomePacchetto()
+    {
+        return $this->nomePacchetto;
+    }
+
+    /**
+     * Set dataInizio
+     *
+     * @param \DateTime $dataInizio
+     *
+     * @return nuovo_pacchetto
+     */
+    public function setDataInizio($dataInizio)
+    {
+        $this->dataInizio = $dataInizio;
+
+        return $this;
+    }
+
+    /**
+     * Get dataInizio
+     *
+     * @return \DateTime
+     */
+    public function getDataInizio()
+    {
+        return $this->dataInizio;
+    }
+
+    /**
+     * Set dataFine
+     *
+     * @param \DateTime $dataFine
+     *
+     * @return nuovo_pacchetto
+     */
+    public function setDataFine($dataFine)
+    {
+        $this->dataFine = $dataFine;
+
+        return $this;
+    }
+
+    /**
+     * Get dataFine
+     *
+     * @return \DateTime
+     */
+    public function getDataFine()
+    {
+        return $this->dataFine;
+    }
+
+    /**
+     * Set servizi
+     *
+     * @param string $servizi
+     *
+     * @return nuovo_pacchetto
+     */
+    public function setServizi($servizi)
+    {
+        $this->servizi = $servizi;
+
+        return $this;
+    }
+
+    /**
+     * Get servizi
+     *
+     * @return string
+     */
+    public function getServizi()
+    {
+        return $this->servizi;
+    }
+}
+
